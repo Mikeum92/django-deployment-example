db3cd507b46914255649241ff192a639c82ea32e
Michael Lontsie <lontmichael@gmail.com>
2020-10-23 15:54:09 +0300

    settings updated

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '%5rsfqox7n!n8*$9s0jfh9*%8*-w!fr^tard2m!=8q(raxj_h7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'basic_app.apps.BasicAppConfig',
+    'basic_app',
 ]
 
 MIDDLEWARE = [
