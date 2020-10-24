e81cb6919f162cc1b2b711550b093f70bb29526f
Michael Lontsie <lontmichael@gmail.com>
2020-10-24 08:42:35 +0300

    settings

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'basic_app.apps.BasicAppConfig',
+    #'basic_app.apps.BasicAppConfig',
     'basic_app',
 ]
 
